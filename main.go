5418d07ac6e52b9ada7d85c15998065d6b4b9db5
ckappgit <171122123+ckappgit@users.noreply.github.com>
2025-01-06 19:33:15 +0800

    feat:完成多线程模板

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
-import "github.com/TensoRaws/ffmpeg_for_BililiveRecorder/webhook"
+import (
+	"fmt"
+	"time"
+
+	"github.com/TensoRaws/ffmpeg_for_BililiveRecorder/webhook"
+)
 
 func main() {
 	wh := webhook.NewWebhookServer("test")
-	wh.OnRecordStart(func(event *webhook.EventRecordStart) {
-		//执行代码
+	wh.OnFileClose(func(event *webhook.EventFileClose) {
+		fmt.Println("已接收待处理文件" + event.EventData.RelativePath)
+		go ffmpegrun(event.EventData.RelativePath)
+		fmt.Println("已接提交文件" + event.EventData.RelativePath)
 	})
 
 	webhook.StartServers(":8080")
+}
+
+func ffmpegrun(path string) {
+	fmt.Println("开始处理" + path)
+	//执行逻辑
+	time.Sleep(time.Second * 60)
+	fmt.Println("完成处理" + path)
 }
